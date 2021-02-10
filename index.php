0edf195c948a372f38a04d586ec5b8132141d335
03242020 <62064207+03242020@users.noreply.github.com>
2021-02-10 14:47:22 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -1,107 +1,85 @@
 <?php
-
-session_start( );
-
-include 'dbFunction.php';
-
-$db = new DBfunctionCLASS();
-
-if (isset($_SESSION['userId'])) {
-        header('Location: ./input.php');
-        exit();
-		}
-
-if (isset($_POST['Login'])) {
-		//シートログイン押下時の記述
-	if ($_POST['Login'] == 'ログイン') {
-
-		$err = "";
-
-		if (isBlank($_POST['userID'])) {
-			$err = "「ID」";
-		}
-
-		if (isBlank($_POST['password'])) {
-			$err .= "「パスワード」";
-		}
-
-		if (isBlank($err)) {
- 			$stmt = $db->getconnection()->prepare("SELECT USER_ID, USER_PASS FROM ACCOUNT");
- 			$stmt->execute();
- 			//$row = $stmt->fetch();
-
- 			if ($row['USER_ID'] == $_POST['userID'] && $row['USER_PASS'] == $_POST['password']) {
-
- 				$_SESSION['userId'] = ACCOUNT.USER_ID;
- 				$_SESSION['userName'] = ACCOUNT.USER_NAME;
- 				$_SESSION['email'] = ACCOUNT.EMAIL;
-
- 				$_SESSION['actionName'] = "index_login";
-
- 				header('Location: ./input.php');  // 一覧画面へ遷移
- 				exit();  // 処理終了
-
- 			}else{
-				$err = "「ID」「パスワード」が存在しません";
-
-			}
-
-		}else{
-			$err .= "が入力されていません";
-		}}
-
-		}
-
-	}else{
-	$_SESSION['actionName'] = "index_display";
+include "common.php";
+$db = new DbconnectClass ();
+@session_start ();
+$other = 'test1';
+$test2 = "";
+if (isset ( $_SESSION ['userid'] )) {
+	header ( 'Location: /input.php' );
+	exit ();
+}
+if (isset ( $_POST ['Login'] )) {
+	if ($_POST ['Login'] == 'ログイン') {
+		// シートログイン押下時の記述
+	} else {
+		$_SESSION ['actionName'] = "index_display";
 	}
+}
 
+$err = "";
 
-// $stmt = $db->prepare("select USER_ID, USER_PASS from ACCOUNT");
-// 		USER_ID == $_POST['userID'] && USER_PASS == $_POST['password'];
+if (isBlank ( $_POST ['userID'] )) {
+	$err = 'ID';
+}
+if (isBlank ( $_POST ['password'] )) {
+	$err .= 'パスワード';
+}
+if (isBlank ( $err )) {
+	// return;
+} else {
+	$err .= "が入力されていません";
+}
+// 入力されたIDとPASSで2箇所PHP側で判断　PASS=で照合
+//$stmt = $db->getdbconnect()->prepare("select * from ACCOUNT group by USER_PASS = :test2");
+//$query = "select from ACCOUNT where USER_ID=:other"
+//select * from ACCOUNT where USER_ID = 'test1';
+//$query = "select * from ACCOUNT where USER_ID=:other";
+$query2 = "select USER_PASS from ACCOUNT where USER_ID = (select USER_ID from ACCOUNT where USER_ID='test1')";
+$stmt = $db->getdbconnect()->prepare($query2);
+$stmt->bindValue(":other","$other",PDO::PARAM_STR);
+//$stmt = $pdo->prepare($query);
+//$stmt = $dbh->prepare('SELECT * FROM ACCOUNT WHERE USER_ID = :other');
+$stmt->execute();
+$count = $stmt ->fetchColumn();
+echo $count;
 
-// $stmt->execute();
-
-// $row = $stmt -> fetchAll(PDO::FETCH_ASSOC);
-
-// $stmt = $db->prepare("select from ACCOUNT WHERE USER_ID = $_POST['userID'] && ("select from ACCOUNT WHERE USER_PASS") = $_POST['password'];
-// $stmt->execute();
-// $row = $stmt->fetch();
-
-
+//$query2 = "select * from ACCOUNT where USER_PASS =:count";
+//$stmt2 = $db->getdbconnect()->prepare($query2);
+//$stmt2->bindValue(":count","$count",PDO::PARAM_STR);
+//$stmt2->execute();
+//$ans = $stmt2->fetchColumn();
+//echo $ans;
+//$name = "\". $name . "\";
+//$stmt->bindParam(':name',$name, PDO::PARAM_STR);
 
 ?>
-
 
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-	<meta charset="UTF-8">
-	<link rel="stylesheet" href="./css/master.css" type="text/css">
-	<title>掲示板</title>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="./css/master.css" type="text/css">
+<title>掲示板</title>
 </head>
 <body>
-<header>
-掲示板
-</header>
-<main>
+	<header> 掲示板 </header>
+	<main>
 	<div>
 		<p>あなたのIDとパスワードを入力してログインしてください。</p>
-		<p><?php echo '<FONT COLOR="RED">'.$err.'</FONT>'; ?></p>
-		<form action="" method="POST" name="Form1">
+		<form action="./input.html" method="post">
 			<p>
-				<label class="itemName">ID:</label>
-				<input type="text" name="userID" value="<?php if (!empty($_POST["userID"])) {echo htmlspecialchars($_POST["userID"], ENT_QUOTES);} ?>">
+				<label class="itemName">ID:</label> <input type="text" name="userID"
+					value="">
 			</p>
 			<p>
-				<label class="itemName">パスワード:</label>
-				<input type="password" name="password" value="">
+				<label class="itemName">パスワード:</label> <input type="password"
+					name="password">
 			</p>
 			<div>
 				<input class="button" type="submit" name="Login" value="ログイン">
 			</div>
 		</form>
 	</div>
-</main>
+	</main>
 </body>
 </html>
