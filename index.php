e6196833c0f2c84d23eed8d1fc763056151a6b7d
TM-IMGY-YEAR <78530039+TM-IMGY-YEAR@users.noreply.github.com>
2021-02-10 11:22:45 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -1,32 +1,61 @@
 <?php
-session_start();
-include "common.php";
 
-//$_SESSION['username'];
-if (isset($_SESSION['userid'])) {
+session_start( );
+
+include 'dbFunction.php';
+
+$db = new DBfunctionCLASS();
+
+if (isset($_SESSION['userId'])) {
         header('Location: /input.php');
         exit();
-    }
-if (isset($_POST['Login'])) {
-	if ($_POST['Login'] == 'ログイン') {
-		//シートログイン押下時の記述
-	}else{
-		$_SESSION['actionName'] = "index_display";
-	}
-}
-$err = "";
+		}
 
-if (isBlank($_POST['userID'])){
-	$err='ID';
+if (isset($_POST['Login'])) {
+		//シートログイン押下時の記述
+	if ($_POST['Login'] == 'ログイン') {
+
+		$err = "";
+
+		if (isBlank($_POST['userID'])) {
+			$err = "「ID」";
+		}
+
+		if (isBlank($_POST['password'])) {
+			$err .= "「パスワード」";
+		}
+
+		if (isBlank($err)) {
+			$stmt = $db->getconnection()->prepare("select USER_ID, USER_PASS from ACCOUNT");
+			$stmt->execute();
+			$row = $stmt->fetch();
+
+			if ($row['USER_ID'] == $_POST['userID'] && $row['USER_PASS'] == $_POST['password']) {
+
+				$_SESSION['userId'] = ACCOUNT.USER_ID;
+				$_SESSION['userName'] = ACCOUNT.USER_NAME;
+				$_SESSION['email'] = ACCOUNT.EMAIL;
+
+				$_SESSION['actionName'] = "index_login";
+
+				header('Location: /input.php');  // メイン画面へ遷移
+				exit();  // 処理終了
+
+			}else{
+				$err = "「ID」「パスワード」が存在しません";
+
+			}
+
+		}else{
+			$err .= "が入力されていません";
+		}
+
+	}
+
+	}else{
+	$_SESSION['actionName'] = "index_display";
 }
-if (isBlank($_POST['password'])){
-	$err.='パスワード';
-}
-if (isBlank($err)){
-	//return;
-}else{
-	$err.="が入力されていません";
-}
+
 
 // $stmt = $db->prepare("select USER_ID, USER_PASS from ACCOUNT");
 // 		USER_ID == $_POST['userID'] && USER_PASS == $_POST['password'];
@@ -38,36 +67,41 @@ if (isBlank($err)){
 // $stmt = $db->prepare("select from ACCOUNT WHERE USER_ID = $_POST['userID'] && ("select from ACCOUNT WHERE USER_PASS") = $_POST['password'];
 // $stmt->execute();
 // $row = $stmt->fetch();
+
+
+
 ?>
+
 
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-  <meta charset="UTF-8">
-  <link rel="stylesheet" href="./css/master.css" type="text/css">
-  <title>掲示板</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="./css/master.css" type="text/css">
+	<title>掲示板</title>
 </head>
 <body>
 <header>
-   掲示板
+掲示板
 </header>
 <main>
-  <div>
-     <p>あなたのIDとパスワードを入力してログインしてください。</p>
-     <form action="./input.html" method="post">
-       <p>
-          <label class="itemName">ID:</label>
-          <input type="text" name="userID" value="">
-       </p>
-       <p>
-          <label class="itemName">パスワード:</label>
-          <input type="password" name="password">
-       </p>
-       <div>
-          <input class="button" type="submit" name="Login" value="ログイン">
-       </div>
-     </form>
-    </div>
+	<div>
+		<p>あなたのIDとパスワードを入力してログインしてください。</p>
+		<p><?php echo '<FONT COLOR="RED">'.$err.'</FONT>'; ?></p>
+		<form action="" method="POST" name="Form1">
+			<p>
+				<label class="itemName">ID:</label>
+				<input type="text" name="userID" value="<?php if (!empty($_POST["userID"])) {echo htmlspecialchars($_POST["userID"], ENT_QUOTES);} ?>">
+			</p>
+			<p>
+				<label class="itemName">パスワード:</label>
+				<input type="password" name="password" value="">
+			</p>
+			<div>
+				<input class="button" type="submit" name="Login" value="ログイン">
+			</div>
+		</form>
+	</div>
 </main>
 </body>
 </html>
