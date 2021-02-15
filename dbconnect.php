aaf8f7e760bbaa56b77988ab001aaba1f115088f
TM-IMGY-YEAR <78530039+TM-IMGY-YEAR@users.noreply.github.com>
2021-02-15 16:31:33 +0900

    Add files via upload

---
+++ dbconnect.php
@@ -0,0 +1,24 @@
+<?php
+
+class DBconnnectCLASS {
+
+public function getconnnect(){
+
+		$dsn = 'mysql:host=localhost;dbname=SLJ;charset=utf8';
+		$user = 'root';
+		$password = 'sljslj';
+
+	try {
+		$db = new PDO($dsn,$user,$password);
+		$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+		return $db;
+
+	} catch (PDOException $e){
+		header('Location: ./error.php');
+	}
+
+	}
+}
+
+
+?>
